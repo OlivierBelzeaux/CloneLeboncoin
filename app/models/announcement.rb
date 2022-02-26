82b6e4a34d2c3ee46936c54ed87a74aac772b5ee
Olivier Belzeaux <olivierbelzeaux@gmail.com>
2022-02-26 14:12:18 +0100

    Announcement validates

--- app/models/announcement.rb
+++ app/models/announcement.rb
@@ -1,3 +1,8 @@
 class Announcement < ApplicationRecord
   belongs_to :user
+  CATEGORIES = ["automobile", "éléctromenager", "ameublement"]
+
+  validates :category, inclusion: { in: CATEGORIES }
+  validates :title, :url_photo, :price, :description, presence: true
+  validates :price, numericality: { greater_than: 0, only_integer: true }
 end
